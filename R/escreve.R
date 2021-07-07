ee43d6c4f8dc432c708dd07bdef5897eccd3fbfd
Athos Damiani <athos.damiani@gmail.com>
2021-07-07 19:45:45 -0300

    subi funcoes soma e escreve

---
+++ R/escreve.R
@@ -0,0 +1,22 @@
+#' Eco
+#'
+#' Repete a mensagem passada.
+#'
+#' @param msg (string) mensagem a ser repetida
+#'
+#' @return (string) mensagem repetida
+#' @export
+#'
+#' @examples
+#'
+#' escreve("aloalo")
+#'
+escreve <- function(msg) {
+  msg
+
+
+
+
+
+
+}
---
+++ R/soma.R
@@ -0,0 +1,20 @@
+#' Soma
+#'
+#' Função que soma dois números.
+#'
+#' @param a (numeric) Um dos números para somar
+#' @param b (numeric) O outro dos números para somar
+#'
+#' @return (numeric) Número resultante da soma de a + b
+#'
+#' @examples
+#'
+#' soma(3, 10)
+#' soma(-2, 2)
+#'
+#' @export
+soma <- function(a, b) {
+  a + b
+}
+
+
